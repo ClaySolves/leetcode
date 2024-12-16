8378b5954be4f8c736f26b1bc577a48b4fa4279d
root <root@DESKTOP-KK7LG21.>
2024-12-15 19:32:01 -0500

    contains duplicate

---
+++ easy/readBinaryWatch.py
@@ -0,0 +1,14 @@
+class Solution:
+    def readBinaryWatch(self, turnedOn: int) -> list[str]:
+        ans = []
+
+
+
+        return ans
+    
+def main():
+    solution = Solution()
+    print(solution.readBinaryWatch(3))
+
+if __name__ == "__main__":
+    main()
