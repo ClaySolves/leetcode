6fd3d8272da72ac342b0620090eedcf9b764f422
root <scarboroclay@gmail.com>
2024-12-16 15:20:05 -0500

    1 medium longestConsecutive

---
+++ medium/longestConsecutive.py
@@ -0,0 +1,12 @@
+class Solution:
+    def longestConsecutive(self, nums: list[int]) -> int:
+        pass
+
+def main():
+    solution = Solution()
+    print(solution.isValidSudoku(
+        [100,4,200,1,3,2]
+        ))
+
+if __name__ == "__main__":
+    main()   
